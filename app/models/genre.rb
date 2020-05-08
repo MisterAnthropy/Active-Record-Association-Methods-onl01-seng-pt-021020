18356c19668ee72b02870bd188f97b097e8b9f1c
Tyler Croley <tjcroley@outlook.com>
2020-05-08 20:17:10 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     artists = self.artists.collect do |artist|
-      self.name = artists
+      artist.name 
+    end
   end
 end
